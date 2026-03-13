a9ac10073f3f720274b1005aea155522a1279e26
sskhilonaghar-cmd <sskhilonaghar@gmail.com>
2026-03-13 14:32:39 +0530

    Update serializers.py

--- shop/serializers.py
+++ shop/serializers.py
@@ -3,15 +3,25 @@ from .models import Product, Order, GalleryImage
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
 
 
 class OrderSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name', read_only=True)
     product_price = serializers.DecimalField(
-        source='product.price', max_digits=10, decimal_places=2, read_only=True
+        source='product.price',
+        max_digits=10,
+        decimal_places=2,
+        read_only=True
     )
 
     class Meta:
@@ -20,6 +30,13 @@ class OrderSerializer(serializers.ModelSerializer):
 
 
 class GalleryImageSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = GalleryImage
         fields = '__all__'
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
